35d705184f21a8eedacfa81fe7c0511201275c64
Bhatt Akshat <akshatbhatt0786@gmail.com>
2026-06-13 15:47:58 +0530

    2 pointer - reverse an array

---
+++ arrays/ReverseAnArray.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class ReverseAnArray 
+{
+
+    static void reverse(int[] arr)
+    {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i <= j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        int[] arr = {1, 2, 3, 4, 5};
+        for(int i: arr)
+            System.out.print(i + " ");
+        System.out.println();
+        reverse(arr);
+        for(int i: arr)
+            System.out.print(i + " ");
+    }
+}
